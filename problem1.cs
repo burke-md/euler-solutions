a14c48dcbca08236d352605080f7e3b80c258bdf
Michael <michael.dburke56@gmail.com>
2021-08-08 18:36:01 -0400

    Solution to problem 1

---
+++ problem1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float runningTotal = 0;
+
+            for (int i = 1; i < 1000; i ++)
+            {
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    runningTotal += i;
+                } 
+            
+            }
+            Console.WriteLine(runningTotal);
+        }
+    }
+}
