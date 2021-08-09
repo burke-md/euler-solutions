a61cc70e7fc9cc16f2026f0c59dc84fdeaf33a74
Michael <michael.dburke56@gmail.com>
2021-08-08 20:06:15 -0400

    Solution to problem 2

---
+++ problem2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int oldI = 1;
+      int nextFib = 0;
+      int evenSum = 0;
+
+      for (int i = 1; i < 4000000; i = nextFib)
+
+      {
+        nextFib = i + oldI;
+        oldI = i;
+
+        if (i % 2 == 0)
+
+        {
+          evenSum += i;
+        }
+
+      }
+
+      Console.WriteLine(evenSum);
+    }
+  }
+}
