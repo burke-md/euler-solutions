c2b7887b221bb8b617821bd56fe8e40f795afb85
Michael <michael.dburke56@gmail.com>
2021-08-13 22:32:07 -0400

    Build prime array. Requires filter for zero.

--- problem3.cs
+++ problem3.cs
@@ -4,17 +4,31 @@ namespace MyApplication
 {
   class Program
   {
-    //create two functions. 1 - create an array of prime numbers 
-    //2- figure out what numbers to best use
+    //1 - Create an array of prime numbers (Sieve of Eratosthennes)
+    //2- Find largest factor.
     static void Main(string[] args)
     {
-      int maxVal = 13195;
+      int maxVal = 30;
       int [] listVals = new int[maxVal];
-      for (var i = 0; i < listVals.Length; i += 1)
+      //Populate array.
+      for (var i = 0; i < listVals.Length; i ++)
+      {
         listVals[i] = i;
-      
+      }
+      var root = Math.Sqrt(maxVal);
+
+      for (var j = 2; j < root; j ++)
+      {
+        for (var k = j * j; k < listVals.Length; k += j)
+        {
+        listVals[k] = 0;
+        }
+
+      }
+
 
       Console.WriteLine(string.Join("\n", listVals));
+      //Filter for non zero values and return
 
       
     }
