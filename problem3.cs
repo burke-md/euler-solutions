df780159c174c4aae750d1fda7527b99824be44c
Michael <michael.dburke56@gmail.com>
2021-08-13 22:08:50 -0400

    Create array. Must impliment sieve and find largest factor.

---
+++ problem3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    //create two functions. 1 - create an array of prime numbers 
+    //2- figure out what numbers to best use
+    static void Main(string[] args)
+    {
+      int maxVal = 13195;
+      int [] listVals = new int[maxVal];
+      for (var i = 0; i < listVals.Length; i += 1)
+        listVals[i] = i;
+      
+
+      Console.WriteLine(string.Join("\n", listVals));
+
+      
+    }
+  }
+}
