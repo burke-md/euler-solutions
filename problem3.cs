bdc119506da426f8ceb885f3388632dcf298d90b
Michael <michael.dburke56@gmail.com>
2021-08-14 20:33:19 -0400

    Solution to problem 3

--- problem3.cs
+++ problem3.cs
@@ -11,19 +11,20 @@ namespace MyApplication
       long largerFactor = 0;
       long [] factors = new long[2];
 
-      //Iterate from 2 through Sqrt of maxium value(findPrimeFactorOf).
-      for (var i = 2; i * i < findPrimeFactorOf; i ++)
+      //Iterate from 2 through Sqrt of maxium value(findPrimeFactorOf). Odd values only.
+      for (long i = 3; i * i < findPrimeFactorOf; i += 2)
       {
         if (findPrimeFactorOf % i == 0)
         {
           factors[0] = i;
           factors[1] = findPrimeFactorOf % i;
-
+          // check factor 0 and factor 1
           for (int k = 0; k < 2; k ++)
           {
             bool isPrime = true;
             for (long j = 2; j * j < factors[k]; j ++)
             {
+              //Check that factor 1/2 are prime.
               if (factors[k] % j == 0)
               {
                 isPrime = false;
