b29f0f39e43bee7e0a52d9f5de0499176ed1ce06
Michael <michael.dburke56@gmail.com>
2021-08-14 21:20:56 -0400

    Initial look at problem 4 w/ brute force method.

---
+++ problem4.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int xValue = 999;
+      int yValue = 999;
+      while (yValue > 0) {
+        int product = xValue * yValue;
+        yValue -= 1;
+        //Make palindrome check
+        
+      }
+    }
+  }
+}
