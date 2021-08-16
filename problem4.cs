4de994979c2c19cde18b25c051de12263e288b5f
Michael <michael.dburke56@gmail.com>
2021-08-15 21:08:27 -0400

    Build out palindrome check logic. Need to adapt iteration.

--- problem4.cs
+++ problem4.cs
@@ -6,13 +6,32 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
+      //Brute force method.
+
+
       int xValue = 999;
       int yValue = 999;
       while (yValue > 0) {
         int product = xValue * yValue;
-        yValue -= 1;
-        //Make palindrome check
+        //Need to nest loops to decrement xValue.
+
+
+        //Convert product to string, reverse and compare.
+        //Abstract palindrome check functionality.
+        string productStr = product.ToString();
+        char [] productArr = productStr.ToCharArray();
+        Array.Reverse(productArr);
+        string productStrRev = productArr.ToString();
+
+        bool isPalindrome = productStr.Equals(productStrRev, StringComparison.OrdinalIgnoreCase);
         
+        //End iteration if palindromic number is reached. 
+       if (isPalindrome) {
+         Console.WriteLine(productStr);
+         break;
+       }
+
+        yValue -= 1;   
       }
     }
   }
