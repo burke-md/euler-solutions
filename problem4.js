39e1dd27e6e4214b3a8b087955f3a7e12e37d636
Michael <michael.dburke56@gmail.com>
2021-11-04 13:42:11 -0400

    Complete helper function, remove comments.

--- problem4.js
+++ problem4.js
@@ -4,12 +4,7 @@ const palindromeCheck = (number) => {
   for (let i = 0; i < numStr.length / 2; i++) {
     const rightIndex = numStr.length - (i + 1);
     const leftIndex = i;
-    console.log(
-      numStr.charAt(leftIndex),
-      numStr.charAt(rightIndex),
-      numStr.charAt(leftIndex) === numStr.charAt(rightIndex) ? true : false
-    );
-
+    if(numStr.charAt(leftIndex) !== numStr.charAt(rightIndex))return false
   }
 
   return true;
@@ -18,4 +13,4 @@ const palindromeCheck = (number) => {
 let x = 999;
 let y = 999;
 
-console.log(palindromeCheck(999));
+console.log(palindromeCheck(9991));
