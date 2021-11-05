3afb7b6a3bd288aa1b82aa961ceaf6adecbd5ec6
Michael <michael.dburke56@gmail.com>
2021-11-05 16:09:33 -0400

    Incorrect answer, needs to be reworked.

--- problem5.js
+++ problem5.js
@@ -1,14 +1,16 @@
 //Smallest positive number that can be divided by each number upto than 20
 
 const findMin = (target) => {
-  let count = 0;
+  let count = 1;
   let min = 1;
-  while(count < target){
+  while (count < target) {
     //brute force increment to 20
     //Optimize in steps pattern after
+    if (target % count !== 0) min *= count;
+
+    count++;
   }
-
-}
-
+  return min;
+};
 
 console.log(findMin(20));
