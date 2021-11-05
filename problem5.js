3f21d31e3272eb7e9f19431a0c095bdd668ee37b
Michael <michael.dburke56@gmail.com>
2021-11-05 12:13:45 -0400

    Incorrect answer.

--- problem5.js
+++ problem5.js
@@ -5,3 +5,14 @@
 
 console.log(17*19)
 //323
+
+//Iterate down from 20, multiple by value if not divisible?
+
+let currentMin = 323; 
+for(let val = 20; val > 0; val --){
+  if (currentMin % val !== 0){
+    currentMin *= val
+    console.log(`${val} does not divide evenly`)
+  }
+}
+console.log(currentMin);
