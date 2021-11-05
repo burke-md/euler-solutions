d31c81e5bc50477171ed6aec9fb38033bfac7cf0
Michael <michael.dburke56@gmail.com>
2021-11-05 15:38:41 -0400

    New method.

--- problem5.js
+++ problem5.js
@@ -1,18 +1,14 @@
 //Smallest positive number that can be divided by each number upto than 20
 
-
-//Prime numbers -> find that 2 largest primes, multiply them together and start there?
-
-console.log(17*19)
-//323
-
-//Iterate down from 20, multiple by value if not divisible?
-
-let currentMin = 323; 
-for(let val = 20; val > 0; val --){
-  if (currentMin % val !== 0){
-    currentMin *= val
-    console.log(`${val} does not divide evenly`)
+const findMin = (target) => {
+  let count = 0;
+  let min = 1;
+  while(count < target){
+    //brute force increment to 20
+    //Optimize in steps pattern after
   }
+
 }
-console.log(currentMin);
+
+
+console.log(findMin(20));
