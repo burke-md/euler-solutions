f0b7e177cbf9bda592a0d990c6ebf1eb404423c5
Michael <michael.dburke56@gmail.com>
2021-11-05 11:59:38 -0400

    New problem, brain storm.

---
+++ problem5.js
@@ -0,0 +1,7 @@
+//Smallest positive number that can be divided by each number upto than 20
+
+
+//Prime numbers -> find that 2 largest primes, multiply them together and start there?
+
+console.log(17*19)
+//323
