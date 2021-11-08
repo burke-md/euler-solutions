6c9bfd1fa5894c0fdf61ed6a3b13662131c43d03
Michael <michael.dburke56@gmail.com>
2021-11-08 09:36:04 -0500

    Initial commit of problem 6.

---
+++ problem6.js
@@ -0,0 +1,7 @@
+/*The sum of the first ten natural numbers squared is 385
+  The square of the first ten natural numbers summed is 3025
+  The difference is 2640
+
+  What is the difference if we are to use the first 100 natural
+  numbers in the above example?
+ */
